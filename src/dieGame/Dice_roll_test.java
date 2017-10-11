7f2122f44055eda3c9d777ab0c51e157e5c81842
Patrick Marcell Madsen <32563752+DtuPat@users.noreply.github.com>
2017-10-11 15:45:00 +0200

    Comments

--- src/dieGame/Dice_roll_test.java
+++ src/dieGame/Dice_roll_test.java
@@ -4,21 +4,22 @@ public class Dice_roll_test {
 
 	public static void main(String[] args) {
 		Die die = new Die();
-		int[] c = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		int[] eyecount = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; //Array der indeholder antallet af gange de forskellige summe forkommer.
 		int numbrolls = 1000;
-		for (int i = 1; i <= numbrolls; i++) {
+		for (int i = 1; i <= numbrolls; i++) { //For loop der looper det antal gange vi gerne vil kaste med terningerne.
 			int r1 = die.roll();
 			int r2 = die.roll();
 			int sum = r1 + r2;
 			
 			// 2 - 12
 			// 0 - c.length-1
-			c[sum-2] += 1;
+			eyecount[sum-2] += 1;
 			
 		}
 
-		for (int i = 0; i < 11; i++)
-			System.out.println("The number of times the sum of the two dice rolled was " + (i + 2) + ": " + c[i]);
+		for (int i = 0; i < 11; i++) { //For loop der printer antallet af gange de forskellige summe forkommer.
+			System.out.println("The number of times the sum of the two dice rolled was " + (i + 2) + ": " + eyecount[i]);
+		}
 	}
 
 }
