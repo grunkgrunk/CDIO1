06ac0e9b4c8c0dd7b749801463e954decff7a5a4
grunkgrunk <danielgrunkin@gmail.com>
2017-10-11 14:54:27 +0200

    Close scanner

--- src/dieGame/Main.java
+++ src/dieGame/Main.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// The the game in terminal.
-		
+		// The game in terminal.
+		// Get the player names. 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Player 1: What is your name? ");
 		String name1 = sc.nextLine();
 		System.out.print("Player 2: What is your name? ");
 		String name2 = sc.nextLine();
-		
+		sc.close();
 		
 		Game game = new Game(name1, name2);
 		game.runNormal();
